f2b8203123ba6215229ab53722b19df71f2308c4
Tiago Pereira <tiagomrp06@gmail.com>
2018-12-16 19:54:45 +0000

    model components added

--- app/src/main/java/g19/li21n/poo/isel/pt/androidsnake/AndroidSnakeController.java
+++ app/src/main/java/g19/li21n/poo/isel/pt/androidsnake/AndroidSnakeController.java
@@ -3,12 +3,16 @@ package g19.li21n.poo.isel.pt.androidsnake;
 import android.app.Activity;
 import android.os.Bundle;
 
-public class MainActivity extends Activity {
+import pt.isel.poo.tile.TilePanel;
+
+public class AndroidSnakeController extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        System.out.println("01");
+
+        final TilePanel panel = findViewById(R.id.panel);
+
     }
 }
