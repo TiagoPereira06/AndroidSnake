4a13c6d835596bff5ea17a7338f500d2284b60a7
Tiago Pereira <tiagomrp06@gmail.com>
2018-12-16 01:02:22 +0000

    first commit

--- app/src/main/java/g19/li21n/poo/isel/pt/androidsnake/MainActivity.java
+++ app/src/main/java/g19/li21n/poo/isel/pt/androidsnake/MainActivity.java
@@ -9,6 +9,6 @@ public class MainActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        System.out.println("teste");
+        System.out.println("01");
     }
 }
